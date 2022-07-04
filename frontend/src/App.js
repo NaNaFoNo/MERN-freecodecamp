659cfc1d89c6c1686228cd50838334e2a3a7152f
Andreas Vieten <cryptonari@outlook.com>
2022-07-04 13:45:27 +0200

    add: Implement layout and data in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import AddReview from './components/add-review';
-import Restaurants from './components/restaurants';
-import RestaurantsList from './components/restaurants-list';
-import Login from './components/login';
+import AddReview from './components/add-review.js';
+import Restaurant from './components/restaurants.js';
+import RestaurantsList from './components/restaurants-list.js';
+import Login from './components/login.js';
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -22,7 +32,7 @@ function App() {
           </li>
           <li>
             { user ? (
-              <a onClick={logout} className="navbar-link" style={{ cursor: "pointer" }}>
+              <a onClick={logout} className="nav-link" style={{ cursor: "pointer" }}>
                 Logout {user.name}
               </a>
             ) : (
@@ -33,8 +43,30 @@ function App() {
           </li>
         </div>
       </nav>
+
+      <div className="container mt-3">
+      
+        <Routes>
+          <Route exact path="/" element={<RestaurantsList/>} />
+          <Route exact path="/restaurants" element={<RestaurantsList/>} />
+          <Route 
+            path="/restaurants/:id/review"
+            element={<AddReview user={user} />}
+          />
+          <Route 
+            path="/restaurants/:id"
+            element={<Restaurant user={user} />}
+          />
+          <Route
+            path="/login"
+            element={<Login login={login}/>}
+          />
+        </Routes>
+      </div>
     </div>
-  );
+  )
+
+
 }
 
 export default App;
